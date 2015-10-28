a64117bfa5448eeb9ca7767447a9fe12ce907443
alacxian <alacxian@gmail.com>
2015-10-28 16:34:16 +0800

    add bitcask success

--- src/bertie.erl
+++ src/bertie.erl
@@ -12,4 +12,19 @@
 %% API
 -export([start/0]).
 
-start() -> io:format("hello my name is bertie~n").
+start() ->
+  Handle = bitcask:open("bertie_db", [read_write]),
+  N = fetch(Handle),
+  store(Handle, N+1),
+  io:format("bertie has been run ~p times~n", [N]),
+  bitcask:close(Handle),
+  init:stop().
+
+store(Handle, N) ->
+  bitcask:put(Handle, <<"bertie_executions">>, term_to_binary(N)).
+
+fetch(Handle) ->
+  case bitcask:get(Handle, <<"bertie_executions">>) of
+    not_found -> 1;
+    {ok, Bin} -> binary_to_term(Bin)
+  end.
