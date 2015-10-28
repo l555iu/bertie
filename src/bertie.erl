77a9dd86445311a86c534e11d620f26f8a6b8c16
jingxian.lzg <jingxian.lzg@taobao.com>
2015-10-28 14:00:42 +0800

    erlang test

---
+++ src/bertie.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author jingxian.lzg
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. 十月 2015 13:56
+%%%-------------------------------------------------------------------
+-module(bertie).
+-author("jingxian.lzg").
+
+%% API
+-export([start/0]).
+
+start() -> io:format("hello my name is bertie~n").
